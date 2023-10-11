84b4a265e16fbe0e945e0767e8a6da14e47b377e
ottoman98 <osmanzxc02@gmail.com>
2023-10-10 21:40:30 -0500

    siu

--- src/components/stories/stories.tsx
+++ src/components/stories/stories.tsx
@@ -1,13 +1,17 @@
+import "../stories/stories.css";
 function Stories() {
   return (
     <>
       <div>
-        <p>
-          Lorem ipsum dolor sit amet, consectetur adipisicing elit. Eligendi
-          officiis eos, a aspernatur nulla iure blanditiis unde eveniet
-          consequuntur perspiciatis fuga quam? Facilis magni, numquam atque
-          omnis quisquam totam. Natus?
-        </p>
+        <h2>Stories</h2>
+        <div className="card">
+          <h3>Numbers</h3>
+          <p>
+            Lorem ipsum dolor sit amet consectetur adipisicing elit. Mollitia
+            nemo maiores saepe corrupt...
+          </p>
+          <i>Level: A1</i>
+        </div>
       </div>
     </>
   );
