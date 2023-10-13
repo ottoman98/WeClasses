b2253b01a1029b5ba20473e0618f6c0fbe7c8cbd
ottoman98 <osmanzxc02@gmail.com>
2023-10-13 13:59:49 -0500

    stories

--- src/App.tsx
+++ src/App.tsx
@@ -4,6 +4,7 @@ import NavBar from "./components/navbar/navbar";
 import Stories from "./components/stories/stories";
 import Login from "./components/loginRegister/login";
 import Register from "./components/loginRegister/register";
+import Story from "./components/story/story";
 
 function App() {
   return (
@@ -15,6 +16,7 @@ function App() {
           <Route path="/stories" element={<Stories />} />
           <Route path="/login" element={<Login />} />
           <Route path="/register" element={<Register />} />
+          <Route path="stories/storydeprueba" element={<Story />} />
         </Routes>
       </BrowserRouter>
     </>
--- src/components/stories/stories.tsx
+++ src/components/stories/stories.tsx
@@ -1,15 +1,17 @@
 import "../stories/stories.css";
+import { Link } from "react-router-dom";
 function Stories() {
   return (
     <>
-      <div>
+      <div className="stories">
         <h2>Stories</h2>
+        <h3>13/10/2023</h3>
+
         <div className="card">
-          <h3>Numbers</h3>
-          <p>
-            Lorem ipsum dolor sit amet consectetur adipisicing elit. Mollitia
-            nemo maiores saepe corrupt...
-          </p>
+          <Link to="storydeprueba">
+            <h3>BirthDay</h3>
+          </Link>
+          <i>My birthday is coming</i>
           <i>Level: A1</i>
         </div>
       </div>
