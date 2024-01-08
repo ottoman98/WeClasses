4aafb0840133fade1ce993aa2ca5ced19d328892
ottoman98 <osmanzxc02@gmail.com>
2024-01-08 18:51:37 -0500

    back to back

--- WeClasses/src/pages/Home.tsx
+++ WeClasses/src/pages/Home.tsx
@@ -5,12 +5,6 @@ function Home() {
   return (
     <>
       <NavBar />
-      <p>
-        Lorem ipsum dolor, sit amet consectetur adipisicing elit. Sed vitae
-        soluta ut nobis vel quasi fugiat aliquid repellat dolores, adipisci
-        itaque quae nesciunt voluptas alias cupiditate, consequuntur iusto
-        repellendus deserunt.
-      </p>
 
       <Outlet />
     </>
