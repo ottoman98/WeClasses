e1a587789f8a20296738051dbb303fea7c37fd7c
ottoman98 <osmanzxc02@gmail.com>
2024-01-10 14:04:55 -0500

    push

--- WeClasses/src/pages/Home.tsx
+++ WeClasses/src/pages/Home.tsx
@@ -2,7 +2,6 @@ import { Outlet } from "react-router-dom";
 import NavBar from "../components/NavBar";
 
 function Home() {
-  console.log(import.meta.env.VITE_URL);
   return (
     <>
       <NavBar />
