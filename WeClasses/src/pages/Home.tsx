b5c069f319519a32e42ba7d510b333f6dbe9dc47
ottoman98 <osmanzxc02@gmail.com>
2024-01-10 14:00:06 -0500

    diploi

--- WeClasses/src/pages/Home.tsx
+++ WeClasses/src/pages/Home.tsx
@@ -2,10 +2,11 @@ import { Outlet } from "react-router-dom";
 import NavBar from "../components/NavBar";
 
 function Home() {
+  console.log(import.meta.env.VITE_URL);
   return (
     <>
       <NavBar />
-
+      AAAA
       <Outlet />
     </>
   );
