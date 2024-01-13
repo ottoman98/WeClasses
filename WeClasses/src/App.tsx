380b3184dde477138155fa48803a726ae2495168
ottoman98 <osmanzxc02@gmail.com>
2024-01-13 13:33:43 -0500

    push

--- WeClasses/src/App.tsx
+++ WeClasses/src/App.tsx
@@ -2,18 +2,13 @@ import { Route, Routes, HashRouter } from "react-router-dom";
 
 import { DataProvider } from "./context/session";
 
-import Leaks from "./components/Leaks";
 import Home from "./pages/Home";
 import Dashboard from "./pages/Dashboard";
 import AllStories from "./components/AllStories";
-import Login from "./components/Login";
-import EditStory from "./components/EditStory";
 import StoriesForm from "./components/StoriesForm";
-import ClientStories from "./components/ClientStories";
 import ProtectedRoutes from "./pages/ProtectedRoutes";
+import EditStory from "./components/EditStory";
 import AllClasses from "./components/AllClasses";
-import StoryBody from "./components/StoryBody";
-import UserRemainingForm from "./components/UserRemainingForm";
 import ClassesForm from "./components/partials/ClassesForm";
 import EditClassesForm from "./components/partials/EditClassForm";
 
@@ -23,14 +18,8 @@ function App() {
       <DataProvider>
         <HashRouter>
           <Routes>
-            <Route path="/" element={<Home />}>
-              <Route path="/login" element={<Login />} />
-              <Route path="/leaks" element={<Leaks />} />
-              <Route path="/story/:language" element={<ClientStories />} />
-              <Route path="/story/:language/:id" element={<StoryBody />} />
-              <Route path="/user/:id" element={<UserRemainingForm />} />
-            </Route>
-
+            <Route path="/" element={<Home />}></Route>
+            //TODO: LOGIN ROUTES
             <Route element={<ProtectedRoutes />}>
               <Route path="/dashboard" element={<Dashboard />}>
                 <Route path="/dashboard/resume" element={<>Stats</>} />
--- WeClasses/src/pages/Home.tsx
+++ WeClasses/src/pages/Home.tsx
@@ -1,11 +1,8 @@
-import { Link, Outlet } from "react-router-dom";
-import NavBar from "../components/NavBar";
+import { Outlet } from "react-router-dom";
 
 function Home() {
   return (
     <>
-      <NavBar />
-      <Link to="/user/12323">link</Link>
       <Outlet />
     </>
   );
