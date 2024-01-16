ce0246d92e2810a7c492d93e8d8808e10c9ffa0a
ottoman98 <osmanzxc02@gmail.com>
2024-01-16 18:57:37 -0500

    footer

--- WeClasses/src/components/v1/Footer.tsx
+++ WeClasses/src/components/v1/Footer.tsx
@@ -41,7 +41,7 @@ function Footer2() {
         </div>
         <Footer.Divider />
         <div className="w-full sm:flex sm:items-center sm:justify-between">
-          <Footer.Copyright href="#" by="Flowbite™" year={2022} />
+          <Footer.Copyright href="#" by="WeClasses" year={2022} />
           <div className="mt-4 flex space-x-6 sm:mt-0 sm:justify-center">
             <Footer.Icon href="#" icon={BsFacebook} />
             <Footer.Icon href="#" icon={BsInstagram} />
