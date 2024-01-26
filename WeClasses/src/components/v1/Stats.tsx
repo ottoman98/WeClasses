0f0df9a9e348323a2a6efdb92386fa0a2389aae0
ottoman98 <osmanzxc02@gmail.com>
2024-01-26 17:14:17 -0500

    commit

--- WeClasses/src/components/v1/Stats.tsx
+++ WeClasses/src/components/v1/Stats.tsx
@@ -6,12 +6,12 @@ function Stats() {
     <section className="flex  p-10 justify-around border-y-2 border-black gap-3 xs:text-[0.7rem] md:text-[1rem] xl:text-[2rem]">
       <div className="flex flex-col">
         <BsPeople />
-        <strong className="xl:text-[0.8em]"> Mas de 200 </strong>
+        <strong className="xl:text-[0.8em]"> +200 </strong>
         <small>estudiantes impactados</small>
       </div>
       <div className="flex flex-col">
         <BsMap />
-        <strong className="xl:text-[0.8em]"> Mas de 3 años </strong>
+        <strong className="xl:text-[0.8em]"> 3 años </strong>
         <small>de experiencia del proyecto</small>
       </div>
       <div className="flex flex-col ">
