7707c3844657593b4ee19471e555e36ac6ccac80
ottoman98 <osmanzxc02@gmail.com>
2024-02-18 16:23:15 -0500

    push data

--- WeClasses/src/components/main/Main.tsx
+++ WeClasses/src/components/main/Main.tsx
@@ -6,17 +6,19 @@ import whatsApp from "../../assets/icons/whatsapp.png";
 import Stats from "./Stats";
 import Offers from "./Offers";
 import BecomeTutor from "./BecomeTutor";
+import AQ from "./AQ";
 
 function Main() {
   return (
     <>
       <CarouselFull />
       <FlagsBar />
-      {/*<Discount />*/}
+      <Banner2 />
       <Stats />
+      {/*<Discount />*/}
       <Offers />
       <BecomeTutor />
-      <Banner2 />
+      <AQ />
       <a
         href="https://api.whatsapp.com/send/?phone=%2B14708120999&text&type=phone_number&app_absent=0"
         target="_blank"
--- WeClasses/src/components/main/Stats.tsx
+++ WeClasses/src/components/main/Stats.tsx
@@ -6,28 +6,27 @@ import { DataContextLanguage } from "../../context/language";
 function Stats() {
   const { translation } = useContext(DataContextLanguage);
   return (
-    <section className="flex  p-10 justify-around border-y-2 border-black gap-3 xs:text-[0.7rem] md:text-[1rem] xl:text-[2rem]">
+    <section className="flex p-10 justify-around   gap-3  ">
       <div className="flex flex-col">
-        <BsPeople />
-        <strong className="xl:text-[0.8em]">
-          {translation.stats.first.bold}
-        </strong>
-        <small>{translation.stats.first.text}</small>
+        <BsPeople size={30} />
+        <strong className="text-xl">{translation.stats.first.bold}</strong>
+        <small className="text-lg text-slate-700">
+          {translation.stats.first.text}
+        </small>
       </div>
       <div className="flex flex-col">
-        <BsMap />
-        <strong className="xl:text-[0.8em]">
-          {translation.stats.second.bold}{" "}
-        </strong>
-        <small>{translation.stats.second.text}</small>
+        <BsMap size={30} />
+        <strong className="text-xl">{translation.stats.second.bold}</strong>
+        <small className="text-lg text-slate-700">
+          {translation.stats.second.text}
+        </small>
       </div>
       <div className="flex flex-col ">
-        <BsGlobe />
-        <strong className="xl:text-[0.8em]">
-          {" "}
-          {translation.stats.third.bold}
-        </strong>
-        <small>{translation.stats.third.text}</small>
+        <BsGlobe size={30} />
+        <strong className="text-xl">{translation.stats.third.bold}</strong>
+        <small className="text-lg text-slate-700">
+          {translation.stats.third.text}
+        </small>
       </div>
     </section>
   );
