63a6a2ed23f40613ce34238fb6b754c76193f0c5
ottoman98 <osmanzxc02@gmail.com>
2024-03-03 10:50:35 -0500

    a

--- WeClasses/src/components/dashboard/student/ClassesList.tsx
+++ WeClasses/src/components/dashboard/student/ClassesList.tsx
@@ -1,10 +1,18 @@
+import { GetAllClasses } from "../../../api/axiosClasses";
+
 function ClassesList() {
+  const classes = GetAllClasses();
+
   return (
     <section>
       <h2>Online English tutors & teachers for private lessons</h2>
       <div>
         <div>filters</div>
-        <div></div>
+        <div>
+          {classes?.map(() => {
+            return <>uwu</>;
+          })}
+        </div>
       </div>
     </section>
   );
