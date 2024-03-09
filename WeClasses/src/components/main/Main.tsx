2b2879f0e53318a45748ec2db5023d5ce066b894
ottoman98 <osmanzxc02@gmail.com>
2024-03-09 12:20:50 -0500

    ta bueno

--- WeClasses/src/components/main/Main.tsx
+++ WeClasses/src/components/main/Main.tsx
@@ -5,7 +5,7 @@ import whatsApp from "../../assets/icons/whatsapp.png";
 import Stats from "./Stats";
 import Offers from "./Offers";
 import BecomeTutor from "./BecomeTutor";
-
+import AQ from "./AQ";
 import MobileBanner from "./MobileBanner";
 import Description from "./Description";
 
@@ -19,7 +19,7 @@ function Main() {
       <Offers />
       <BecomeTutor />
       <MobileBanner />
-
+      <AQ />
       <a
         href="https://api.whatsapp.com/send/?phone=%2B14708120999&text&type=phone_number&app_absent=0"
         target="_blank"
