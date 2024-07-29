d52a6c93c0e669bd4b5aec4542c47bc681b854ca
ottoman98 <osmanzxc02@gmail.com>
2024-07-29 09:40:08 -0500

    push

--- WeClasses/src/components/partials/settings/Account.tsx
+++ WeClasses/src/components/partials/settings/Account.tsx
@@ -90,6 +90,7 @@ function Account() {
             className="border-2 placeholder-slate-300 border-slate-200 hover:border-blue-900 focus:to-blue-950 rounded-xl text-xs md:text-base"
             id="name"
             type="file"
+            accept="image/*"
           />
 
           <p className="text-xs italic text-red-500">{errors.photo?.message}</p>
